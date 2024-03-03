3c2c7cb94c9a752bdafb9348e612ae06be85159d
fmtawhid <tawhid0406@gmail.com>
2024-03-03 14:16:26 +0600

    .

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'staticfiles_build', 'static'
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
